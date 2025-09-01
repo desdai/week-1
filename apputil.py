8298d557939c47ea86170498d31daff122c46a12
Bofu Dong <93793232+desdai@users.noreply.github.com>
2025-09-01 20:46:15 +0000

    finish homework

--- apputil.py
+++ apputil.py
@@ -1,3 +1,21 @@
 
 
 # add code below ...
+def palindrome(word):
+    cleaned = ""
+    for ch in word:
+        # Keep lowercase letters a–z
+        if "a" <= ch.lower() <= "z" or "0" <= ch <= "9":
+            cleaned += ch.lower()
+    return cleaned == cleaned[::-1]
+
+def parentheses(sequence):
+    stack = []
+    for char in sequence:
+        if char == "(":
+            stack.append(char)
+        elif char == ")":
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
